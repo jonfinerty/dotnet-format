848ec0ee2c356c9e8533199a742a95b27384f3b7
Jon Finerty <jonathanfinerty@gmail.com>
2018-11-04 14:28:01 +0000

    Check checksum when downloading

--- src/dotnet-format/ResharperCommandLineTools.cs
+++ src/dotnet-format/ResharperCommandLineTools.cs
@@ -3,68 +3,74 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace JonFinerty.DotNetFormat
 {
-    //TODO checksum zip
     public class ResharperCommandLineTools
     {
-        private static readonly string installLocation = Path.Combine(AppContext.BaseDirectory, ".resharper");
-        private readonly string executable = Path.Combine(installLocation, "cleanupcode.exe");
-
-        private readonly string url =
+        private const string ToolsDownloadUrl =
             "https://download.jetbrains.com/resharper/ReSharperUltimate.2018.2.3/JetBrains.ReSharper.CommandLineTools.2018.2.3.zip";
 
-        private readonly string zip = Path.Combine(installLocation,
+        private const string Checksum = "83b160e7d3e4fd4cef4f250e5f1ccb295a6e7a23c673ae0f50fa9ece1667c6d3";
+
+        private static readonly string InstallPath = Path.Combine(AppContext.BaseDirectory, ".resharper");
+        private readonly string _executablePath = Path.Combine(InstallPath, "cleanupcode.exe");
+
+        private readonly string _zipPath = Path.Combine(InstallPath,
             "JetBrains.ReSharper.CommandLineTools.2018.2.3.zip");
 
 
         public bool IsInstalled()
         {
-            Console.WriteLine($"Checking for tools in {executable}");
-            var executableFound = File.Exists(executable);
+            Console.WriteLine($"Checking for tools in {_executablePath}");
+            var executableFound = File.Exists(_executablePath);
             Console.WriteLine(executableFound ? "Executable found" : "Executable not found");
             return executableFound;
         }
 
         public async Task Install()
         {
-            Console.WriteLine($"Downloading to {zip}");
-            Directory.CreateDirectory(installLocation);
-
-            var totalReads = 0L;
-            var buffer = new byte[8192];
+            Console.WriteLine($"Downloading to {_zipPath}");
+            Directory.CreateDirectory(InstallPath);
 
             using (var client = new HttpClient())
             {
-                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var request = new HttpRequestMessage(HttpMethod.Get, ToolsDownloadUrl);
 
                 using (var response = await client.SendAsync(request))
-                using (var stream = await response.Content.ReadAsStreamAsync())
-                using (var fs = new FileStream(zip, FileMode.Create, FileAccess.Write, FileShare.None, buffer.Length))
+                using (var responseContentStream = await response.Content.ReadAsStreamAsync())
+                using (var fileStream = new FileStream(_zipPath, FileMode.Create, FileAccess.Write))
                 {
-                    int read;
-                    while ((read = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
-                    {
-                        await fs.WriteAsync(buffer, 0, read);
-
-                        totalReads += 1;
-
-                        if (totalReads % 64 == 0) Console.Write('.');
-                    }
+                    await responseContentStream.CopyToAsync(fileStream);
                 }
             }
 
             Console.WriteLine();
-
+            CheckCheckSum();
             Unzip();
+        }
+
+        private void CheckCheckSum()
+        {
+            using (var zipStream = new FileStream(_zipPath, FileMode.Open, FileAccess.Read))
+            {
+                var sha256 = SHA256.Create();
+
+                var byteHash = sha256.ComputeHash(zipStream);
+                var hexCheckSum = string.Concat(Array.ConvertAll(byteHash, x => x.ToString("X2")));
+                if (!hexCheckSum.Equals(Checksum, StringComparison.OrdinalIgnoreCase))
+                {
+                    throw new InvalidDataException("Downloaded zip file checksum failed");
+                }
+            }
         }
 
         private void Unzip()
         {
             Console.WriteLine("Unzipping tools");
-            ZipFile.ExtractToDirectory(zip, installLocation, true);
+            ZipFile.ExtractToDirectory(_zipPath, InstallPath, true);
         }
 
         public void CleanupCode(string solution)
@@ -76,7 +82,7 @@ namespace JonFinerty.DotNetFormat
                 StartInfo =
                 {
                     UseShellExecute = false,
-                    FileName = executable,
+                    FileName = _executablePath,
                     CreateNoWindow = true,
                     Arguments = arguments,
                     RedirectStandardOutput = true,
